a7b283e1d822a04997a85ade16a93e1fe385894a
fedelee <39679176+fedelee@users.noreply.github.com>
2020-06-28 11:22:34 +0300

    sort courses by number of users enrolled with mongoose .sort()

--- controllers/home/index.js
+++ controllers/home/index.js
@@ -4,12 +4,21 @@ module.exports = {
     get: {
         async home(req, res, next) {
             const isLoggedIn = req.user !== undefined;
-            
+
             const limit = isLoggedIn ? 0 : 3;
-            const criteria = isLoggedIn ? { createdAt: '-1' } : { enrolledUsers: -1 };
-            const courses = await Course.find({isPublic :true})
-                .sort(criteria).limit(limit).lean();
-           //attach isLoggedIn to each course in courses
+            const criteria = isLoggedIn ? { createdAt: '-1' } : { usersEnrolled: -1 };
+            const courses = await Course
+                .find({ isPublic: true })
+             .sort(criteria)
+            .limit(limit)
+            .lean();
+
+           // if (isLoggedIn) {
+           //     courses.sort((a, b) => b.enrolledUsers.length - a.enrolledUsers.length);
+           // } else {
+           //     courses.sort((a, b) =>new Date( a.createdAt) - new Date(b.createdAt));
+           // }
+            //attached isLoggedIn to each course in courses
             const coursesWithIsLoggedIn = courses.reduce((acc, curr) => {
                 acc.push({ ...curr, isLoggedIn });
                 return acc;
